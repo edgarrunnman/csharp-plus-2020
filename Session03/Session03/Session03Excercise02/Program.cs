b94d1fa552469eb87f5a0569484e89d13b19bd70
Edgar Runnman <edgar.runnman@gmail.com>
2020-08-28 10:11:33 +0200

    v1 skapad

--- Session03/Session03/Session03Excercise02/Program.cs
+++ Session03/Session03/Session03Excercise02/Program.cs
@@ -8,13 +8,27 @@ namespace Session03Excercise02
         {
             Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
             var input = Console.ReadLine();
-            var inputArray = input.Split(new [] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-
+            var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            var sum = 0m;
+            int highestValue = Convert.ToInt32(inputArray[0]);
+            int lowestValue = Convert.ToInt32(inputArray[0]);
             foreach (var number in inputArray)
             {
-                Console.WriteLine("Värdet är " + number);
-            }
+                int numberInteger = Convert.ToInt32(number);
+                sum += numberInteger;
 
+                foreach (var number2 in inputArray)
+                {
+                    int numberInteger2 = Convert.ToInt32(number2);
+                    if ((numberInteger > numberInteger2) & (numberInteger > highestValue)) highestValue = numberInteger;
+                    if ((numberInteger < numberInteger2) & (numberInteger < lowestValue)) lowestValue = numberInteger;
+                }
+            }
+            var average = sum / inputArray.Length;
+            Console.WriteLine("medelvärde är: " + average);
+            Console.WriteLine("högsta värde är: " + highestValue);
+            Console.WriteLine("lägsta värde är: " + lowestValue);
+            Console.ReadKey();
         }
     }
 }
