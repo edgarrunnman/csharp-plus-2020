52962727041e1d7de5b83735534460bd8209f9e8
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-01 11:29:09 +0200

    progess on NumbersApp

--- Session03/Session03/Session03Excercise02/Program.cs
+++ Session03/Session03/Session03Excercise02/Program.cs
@@ -8,48 +8,57 @@ namespace Session03Excercise02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
-            string input = Console.ReadLine();
-            string[] inputArray = input.Split(".", StringSplitOptions.RemoveEmptyEntries);
-            double sum = 0;
-            double highestValue = 0;
-            double lowestValue = 0;
-            double?[] numberArray = new double?[inputArray.Length];
-            for (int i = 0; i < inputArray.Length; i++)
-            {
-                bool parsed = double.TryParse(inputArray[i], out double parsedValue);
+            // Konvertera det inmatade värdena från strängar till siffror
+            // Resultatet skall presentera:
+            // Lägsta värdet
+            // Högsta värdet
+            // Medelvärde (snitt)
+            // Programmet skall vara felhanterat
+            // Felaktiga värden får inte påverka medelvärde, lägsta eller högsta.
 
-                if (parsed)
-                { 
-                    Console.WriteLine(parsedValue);
-                    highestValue = parsedValue;
-                    lowestValue = parsedValue;
-                    numberArray[i] = parsedValue;
-                }
-                else numberArray[i] = null;
-
-            }
-            catch (Exception ex)
+            while (true)
             {
-                exceptionResult = ex.HResult;
+                Console.WriteLine("Ange ett antal siffror, separerat med mellanslag.");
+                string input = Console.ReadLine();
+                string[] inputArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                double sum = 0;
+                double highestValue = 0;
+                double lowestValue = 0;
+                int countOfNumbers = 0;
+                double?[] numberArray = new double?[inputArray.Length];
 
-            foreach (var number in numberArray)
-            {
-                if (number != null)
+                for (int i = 0; i < inputArray.Length; i++)
                 {
-                    sum += (double)number;
-                    foreach (var number2 in numberArray)
+                    bool parsed = double.TryParse(inputArray[i], out double parsedValue);
+
+                    if (parsed)
+                    { 
+                        highestValue = parsedValue;
+                        lowestValue = parsedValue;
+                        numberArray[i] = parsedValue;
+                        countOfNumbers++;
+                    }
+                    else numberArray[i] = null;
+
+                }
+
+                foreach (var number in numberArray)
+                {
+                    if (number != null)
                     {
-                        if ((number > number2) & (number > highestValue)) highestValue = (double)number;
-                        if ((number < number2) & (number < lowestValue)) lowestValue = (double)number;
+                        sum += (double)number;
+                        foreach (var number2 in numberArray)
+                        {
+                            if ((number > number2) && (number > highestValue)) highestValue = (double)number;
+                            if ((number < number2) && (number < lowestValue)) lowestValue = (double)number;
+                        }
                     }
                 }
+                double average = sum / countOfNumbers;
+                Console.WriteLine("medelvärde är: " + average);
+                Console.WriteLine("högsta värde är: " + highestValue);
+                Console.WriteLine("lägsta värde är: " + lowestValue);
             }
-            double average = sum / inputArray.Length;
-            Console.WriteLine("medelvärde är: " + average);
-            Console.WriteLine("högsta värde är: " + highestValue);
-            Console.WriteLine("lägsta värde är: " + lowestValue);
-            Console.ReadKey();
         }
     }
 }
